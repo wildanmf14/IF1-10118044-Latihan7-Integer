d7facf095323be6250e512db2af6bf69b6e3fb67
Wildhevire <wildhevire404@gmail.com>
2019-09-26 17:36:32 +0000

    finished

---
+++ src/latihan/integer/Main.java
@@ -0,0 +1,24 @@
+package latihan.integer;
+
+public class Main {
+	/*
+    Nama        : Wildan Muhammad Fikri
+    Kelas       : IF1
+    NIM         : 10118044
+    Deskripsi   : mendeklarasikan variabel integer
+     */
+	public static void main(String[] args) {
+        byte b;
+        short s;
+        int i;
+        long l;
+        b = 120;
+        s = 32767;
+        i = 1_000_000_000;
+        l = 90000000000l;
+        System.out.println("byte : " + b);
+        System.out.println("short : " + s);
+        System.out.println("integer : " + i);
+        System.out.println("long : " + l);
+    }
+}
